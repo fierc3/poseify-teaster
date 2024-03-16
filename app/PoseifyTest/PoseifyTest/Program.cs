b72587c559e84dcc29c271992e94ef4ba483133f
Mike Pullen <mike.pullen@swissgrc.com>
2024-03-16 14:27:35 +0100

    Add rabbitmq setup for dev containers

---
+++ app/PoseifyTest/PoseifyTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using PoseifyTest;
+
+string hostName = "rabbitmq"; 
+string queueName = "video2pose";
+string message = "Ready";
+string messageType = "videopose3d";
+
+Console.WriteLine("Press 'x' to send a message. Press 'q' to quit.");
+
+var sender = new RabbitMQSender();
+
+while (true)
+{
+    var key = Console.ReadKey(intercept: true).Key;
+    if (key == ConsoleKey.X)
+    {
+        string guid = Guid.NewGuid().ToString();
+        sender.SendMessage(hostName, queueName, message, guid, messageType);
+    }
+    else if (key == ConsoleKey.Q)
+    {
+        break;
+    }
+}
---
+++ app/PoseifyTest/PoseifyTest/RabbitMQSender.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Collections.Generic;
+
+namespace PoseifyTest
+{
+    public class RabbitMQSender
+    {
+        public void SendMessage(string hostName, string queueName, string message, string guid, string messageType)
+        {
+            var factory = new ConnectionFactory() { HostName = hostName, UserName = "user", Password = "password" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName,
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.Headers = new Dictionary<string, object>
+            {
+                { "GUID", guid },
+                { "Type", messageType }
+            };
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: queueName,
+                                     basicProperties: properties,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+    }
+}
